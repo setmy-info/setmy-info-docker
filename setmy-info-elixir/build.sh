274438f82e4763bfa9aa99778f6a73b98a4bbed1
Imre Tabur <imre.tabur@mail.ee>
2026-08-22 19:34:09 +0300

    Elixir Docker image added

---
+++ setmy-info-elixir/build.sh
@@ -0,0 +1,21 @@
+includePackages lfe
+
+ADDITIONAL_VERSION=1
+DOCKER_PROJECT_NAME=setmy-info-elixir
+DOCKER_PROJECT_VERSION=${LFE_VERSION}-${ADDITIONAL_VERSION}
+DOCKER_ID_ORGANIZATION=setmyinfo
+DOCKER_CONTENT_TRUST=1
+
+SMI_HOME_PACKAGES_LOCATION=$(smi-home-packages-location)
+
+docker_prepare() {
+    CUR_DIR=$(pwd)
+    # Erlang, Elixir, and LFE are downloaded and built inside the Docker BUILD stage.
+    cd ${CUR_DIR}
+}
+
+docker_build() {
+    CUR_DIR=$(pwd)
+    # Nothing here right now
+    cd ${CUR_DIR}
+}
